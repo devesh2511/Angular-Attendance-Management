c665b46d76154bb7c3eea5bab7478271c8f8ea68
Devesh Jain <76024137+devesh2511@users.noreply.github.com>
2023-11-08 22:13:35 +0530

    update2

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'student-management-app'`, () => {
+  it(`should have as title 'studAngular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('student-management-app');
+    expect(app.title).toEqual('studAngular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('student-management-app app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('studAngular app is running!');
   });
 });
